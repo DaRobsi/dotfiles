2739f5f3ceec3f7bb241d95ac66450c279aed5a1
DaRobsi <alfarobin388@gmail.com>
2024-06-20 09:51:20 +0200

    enhance swaylock config

--- .config/hypr/scripts/setwallpaper.sh
+++ .config/hypr/scripts/setwallpaper.sh
@@ -8,9 +8,9 @@ killall -9 -q rwpspread
 hour=$(date +"%H")
 
 if [[ $hour -le 6 || $hour -ge 20 ]]; then
-	rwpspread -dp --backend hyprpaper --image /home/$USER/Wallpapers/night
+	rwpspread -dp --backend hyprpaper --locker swaylock --image /home/$USER/Wallpapers/night
 else
-	rwpspread -dp --backend hyprpaper --image /home/$USER/Wallpapers/day
+rwpspread -dp --backend hyprpaper --locker swaylock --image /home/$USER/Wallpapers/day
 fi
 
 dunstify "setwallpaper.sh" "Wallpaper set!"
--- .config/scripts/swaylock.sh
+++ .config/scripts/swaylock.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
 
+CONFIG="$(cat /home/$USER/.cache/rwpspread/rwps_swaylock.conf)"
+
 swaylock -l \
   --ring-color blue \
-  --text-wrong-color red
+  --text-wrong-color red \
+  --indicator-radius 250 \
+  --indicator-thickness 7 \
+  --inside-color 00000000 \
+  --layout-border-color gray \
+  --font-size 30 \
+  $CONFIG
