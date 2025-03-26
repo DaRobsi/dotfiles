35bef369bda4adb013d28ea8787fee88bb587714
DaRobsi <alfarobin388@gmail.com>
2025-03-26 14:18:50 +0100

    some more making it work

--- .config/hypr/scripts/changeTilingRatio.sh
+++ .config/hypr/scripts/changeTilingRatio.sh
@@ -3,9 +3,11 @@
 set -uo pipefail
 
 SPLIT_RATIO=$(hyprctl getoption dwindle:split_width_multiplier | grep float | awk '{print $2}')
-echo $SPLIT_RATIO
+echo $(hyprctl getoption general:layout)
 
-if [[ $(echo "$SPLIT_RATIO==1.0" | bc -l) == 1 ]]; then
+if [[ $(hyprctl getoption general:layout | grep str) == "str: master" ]]; then
+    MSG="Layout must be dwindle!"
+elif [[ $(echo "$SPLIT_RATIO==1.0" | bc -l) == 1 ]]; then
     hyprctl keyword dwindle:split_width_multiplier 1.8
     MSG="Set split ratio to 1.8"
 elif [[ $(echo "$SPLIT_RATIO==1.8" | bc -l) == 1 ]]; then
--- .config/hypr/scripts/setTilingLayout.sh
+++ .config/hypr/scripts/setTilingLayout.sh
@@ -3,14 +3,11 @@
 set -uo pipefail
 
 LAYOUT=$(hyprctl getoption general:layout | grep str)
-echo $LAYOUT
 
 if [[ $LAYOUT == "str: dwindle" ]]; then
-	echo $LAYOUT
 	hyprctl keyword general:layout master
 	dunstify -r 66001 -u low "Hyprland Layout" "Set to Master"
 elif [[ $LAYOUT == "str: master" ]]; then
-	echo $LAYOUT
 	hyprctl keyword general:layout dwindle
 	dunstify -r 66002 -u low "Hyprland Layout" "Set to Dwindle"
 fi
