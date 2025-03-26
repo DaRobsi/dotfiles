a51decbe751f23a18048e1ff07a7e0fb14da9a64
DaRobsi <alfarobin388@gmail.com>
2025-03-26 14:07:30 +0100

    made changeTilingRatio.sh function finally

--- .config/hypr/scripts/changeTilingRatio.sh
+++ .config/hypr/scripts/changeTilingRatio.sh
@@ -2,15 +2,14 @@
 
 set -uo pipefail
 
-SPLIT_RATIO=$(hyprctl getoption dwindle:split_width_multiplier | grep float | awk '{print $2}' | awk '{printf "%.1f\n", $1}')
+SPLIT_RATIO=$(hyprctl getoption dwindle:split_width_multiplier | grep float | awk '{print $2}')
+echo $SPLIT_RATIO
 
-if [[ $(echo "$SPLIT_RATIO==1.0" | bc -l) && "$LAYOUT" = "dwindle" ]]; then
+if [[ $(echo "$SPLIT_RATIO==1.0" | bc -l) == 1 ]]; then
     hyprctl keyword dwindle:split_width_multiplier 1.8
-    SPLIT_RATIO=1.8
     MSG="Set split ratio to 1.8"
-elif [[ $(echo "$SPLIT_RATIO==1.8" | bc -l) && "$LAYOUT" = "dwindle" ]]; then
+elif [[ $(echo "$SPLIT_RATIO==1.8" | bc -l) == 1 ]]; then
     hyprctl keyword dwindle:split_width_multiplier 1.0
-    SPLIT_RATIO=1.0
     MSG="Set split ratio to 1.0"
 else
     MSG="Could not set split ratio!"
